a85716ed0ba4f5c4f266f7fd9c2bd8369d2adfa5
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2024-02-06 00:04:59 +0900

    [Silver II] Title: 이동하기, Time: 784 ms, Memory: 39128 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -5,14 +5,14 @@ import sys
 N, M = map(int, input().split())
 
 lst = []
-dp = [[0] * (M + 1)] * (N + 1)
+dp = [[0] * (M + 1)] * (N + 1) # memoization
 
 for i in range(N):
     tmp = list(map(int, input().split()))
     lst.append(tmp)
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + lst[i-1][j-1]
-        
+for i in range(N):
+    for j in range(M):
+        dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j], dp[i][j]) + lst[i][j]
+
 print(dp[N][M])
