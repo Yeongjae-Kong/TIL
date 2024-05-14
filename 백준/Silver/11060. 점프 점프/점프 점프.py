7d1a165c4ae7d945384ed199379ebd5d06faad98
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2024-05-14 14:06:35 +0900

    [Silver II] Title: 점프 점프, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -1,15 +1,14 @@
 n = int(input())
-jump = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-dp = [n + 1] * n
-dp[0] = 0
+dp = [1001]*(n)
+dp[0]=0
 
 for i in range(n):
-    for j in range(1, jump[i] + 1):
-        if i + j < n:
-            dp[i + j] = min(dp[i + j], dp[i] + 1)
-
-if dp[n - 1] == n + 1:
-	print(-1)
+    for j in range(1, arr[i]+1):
+        if i+j < n:
+            dp[i+j] = min(dp[i+j], dp[i]+1)
+if dp[n-1] == 1001:
+    print(-1)
 else:
-	print(dp[n - 1])
+    print(dp[n-1])
