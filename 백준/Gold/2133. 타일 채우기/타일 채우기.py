287cb3a43024cb2318fb450f69c06d7e3a59205e
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2024-09-04 19:10:23 +0900

    [Gold IV] Title: 타일 채우기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,11 +1,16 @@
 n = int(input())
 
-dp = [0] * 30
+dp = [0]*30
 
 dp[1] = 3
-dp[3] = 11
-for i in range(4, n):
-    if i%2 == 1: # 6, 8, ..
+
+for i in range(2, n):
+    if i%2 == 1:
         dp[i] = dp[i-2]*3 + sum(dp[:i-2])*2 + 2
 
 print(dp[n-1])
+
+
+
+
+
