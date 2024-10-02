2d3ce01b30dfb21ce75051bd87ff454e980ac7fe
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2024-10-02 21:56:13 +0900

    [Silver II] Title: 섬의 개수, Time: 68 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,33 +1,36 @@
-# 대각선 주의
-
 from collections import deque
 
-def bfs(x, y):
-  dx = [1, -1, 0, 0, 1, -1, 1, -1]
-  dy = [0, 0, -1, 1, -1, 1, 1, -1]
-  lst[x][y] = 0
-  q = deque()
-  q.append([x, y])
-  while q:
-    a, b = q.popleft()
-    for i in range(8):
-      nx = a + dx[i]
-      ny = b + dy[i]
-      if 0 <= nx < h and 0 <= ny < w and lst[nx][ny] == 1:
-        lst[nx][ny] = 0
-        q.append([nx, ny])
+ans = []
+
+def bfs(i, j):
+    q = deque()
+    q.append((i,j))
+    while q:
+        a,b = q.popleft()
+        for dx, dy in d:
+            nx = b+dx
+            ny = a+dy
+            if 0<=ny<y and 0<=nx<x and lst[ny][nx]==1:
+                lst[ny][nx] = 0
+                q.append((ny, nx))
+
+d = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
 
 while True:
-  w, h = map(int, input().split())
-  if w == 0 and h == 0:
-    break
-  lst = []
-  count = 0
-  for _ in range(h):
-    lst.append(list(map(int, input().split())))
-  for i in range(h):
-    for j in range(w):
-      if lst[i][j] == 1:
-        bfs(i, j)
-        count += 1
-  print(count)
+    x,y = map(int, input().split())
+    if x==0 and y==0:
+        break
+    else:
+        cnt=0
+        lst = []
+        for _ in range(y):
+            lst.append(list(map(int,input().split())))
+        for i in range(y):
+            for j in range(x):
+                if lst[i][j] == 1:
+                    bfs(i, j)
+                    cnt+=1
+        ans.append(cnt)
+
+for i in range(len(ans)):
+    print(ans[i])
