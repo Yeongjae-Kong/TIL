45151a2cbe14ac0d03161b01879f6b74a7b6eca9
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2024-10-05 22:27:51 +0900

    [Gold III] Title: 내리막 길, Time: 124 ms, Memory: 44200 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,20 +1,25 @@
 import sys
-sys.setrecursionlimit(10 ** 9)
+sys.setrecursionlimit(10**9)
 
-n, m = map(int, input().split())
-lst = [list(map(int, input().split())) for _ in range(n)]
+m, n = map(int, input().split())
 
-d = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 이동 방향: 동, 남, 서, 북
-dp = [[-1]*m for _ in range(n)]
-def dfs(x, y):
-    if x==m-1 and y==n-1:
+lst = []
+for i in range(m):
+    lst.append(list(map(int, input().split())))
+
+d = [(0,1),(1,0),(0,-1),(-1,0)]
+
+dp = [[-1]*n for i in range(m)]
+def dfs(x,y):
+    if x==n-1 and y==m-1:
         return 1
     if dp[y][x] != -1:
         return dp[y][x]
     dp[y][x] = 0
     for dx, dy in d:
         nx, ny = x+dx, y+dy
-        if 0 <= nx < m and 0 <= ny < n and lst[y][x] > lst[ny][nx]:
+        if 0<=nx<n and 0<=ny<m and lst[y][x] > lst[ny][nx]:
             dp[y][x] += dfs(nx, ny)
     return dp[y][x]
-print(dfs(0,0))
+dfs(0,0)
+print(dp[0][0])
