95e9f2562cd9748c91920ad37b2efe75468b2642
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2025-03-04 15:52:56 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,40 @@
 from collections import deque
 
 n = int(input())
-lst = [0] * n
-for i in range(n):
-    lst[i] = list(map(int, input().strip()))
-    
-d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    lst[x][y] = 0
-    count = 1
+lst=[]
+
+for _ in range(n):
+    lst.append(list(map(int, input())))
+
+q = deque()
+visited = [[0]*n for _ in range(n)]
+d = [(0,1),(1,0),(0,-1),(-1,0)]
+
+def bfs(y,x):
+    num = 1 
+    q.append((y,x))
+    visited[y][x] = 1
     while q:
-        x, y = q.popleft()
+        y,x = q.popleft()
         for dx, dy in d:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < n and 0 <= ny < n and lst[nx][ny] == 1:
-                q.append((nx, ny))
-                lst[nx][ny] = 0
-                count += 1
-    return count
-
-ans = []
+            ny, nx = y+dy, x+dx
+            if 0<=ny<n and 0<=nx<n and lst[ny][nx] == 1 and not visited[ny][nx]:
+                q.append((ny, nx))
+                visited[ny][nx] = 1
+                num+=1
+    return num
+cnt = 0
+num_house = []
 for i in range(n):
     for j in range(n):
-        if lst[i][j] == 1:
-            tmp = bfs(i, j)
-            ans.append(tmp)
-ans.sort()
+        if lst[i][j] == 1 and not visited[i][j]:
+            temp = bfs(i,j)
+            num_house.append(temp)
+            cnt+=1
+            
+num_house.sort()
 
-print(len(ans))
-for i in range(len(ans)):
-    print(ans[i])
+print(cnt)
+for num in num_house:
+    print(num)
