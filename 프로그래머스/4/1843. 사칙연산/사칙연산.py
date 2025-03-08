f23454e296c9405db6dc7ba8b294cfb63a665d7c
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2025-03-08 13:06:17 +0900

    [level 4] Title: 사칙연산, Time: 3.80 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,23 +1,13 @@
 def solution(arr):
-    n = (len(arr) + 1) // 2 
+    arrs = ''.join(arr).split('-')
+    val0 = sum(list(map(int, arrs[0].split('+'))))
 
-    dp_max = [[-float('inf')] * n for _ in range(n)]
-    dp_min = [[float('inf')] * n for _ in range(n)]
+    min_val = 0
+    max_val = 0
+    for arr in arrs[:0:-1]:
+        x = list(map(int, arr.split('+')))
+        _min_val = -(sum(x))
+        _max_val = sum(x[1:]) - x[0]
+        min_val, max_val = min(_min_val + min_val, _min_val - max_val), max(_max_val + max_val, _min_val - min_val)
 
-    for i in range(n):
-        dp_max[i][i] = dp_min[i][i] = int(arr[i * 2])
-
-    for length in range(2, n + 1): 
-        for i in range(n - length + 1):
-            j = i + length - 1
-            for k in range(i, j):
-                op = arr[k * 2 + 1] 
-
-                if op == '+':
-                    dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] + dp_max[k+1][j])
-                    dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] + dp_min[k+1][j])
-                else:  # '-'
-                    dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] - dp_min[k+1][j])
-                    dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] - dp_max[k+1][j])
-
-    return dp_max[0][n-1]
+    return val0 + max_val
