94158d2760a37bc3cc66c34b7366763b18871ec5
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2025-03-10 23:54:50 +0900

    [level 1] Title: 소수 만들기, Time: 203.36 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,17 +1,14 @@
 from itertools import combinations
-import math
-
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):  # √n까지만 확인
-        if n % i == 0:
-            return False
-    return True
 
 def solution(nums):
-    prime_count = 0
+    temp = []
     for a, b, c in combinations(nums, 3):
-        if is_prime(a + b + c):
-            prime_count += 1
-    return prime_count
+        temp.append(a+b+c)
+    cnt = len(temp)
+    for i in temp:
+        for j in range(2, i):
+            if i % j == 0:
+                cnt -= 1
+                break
+
+    return cnt
