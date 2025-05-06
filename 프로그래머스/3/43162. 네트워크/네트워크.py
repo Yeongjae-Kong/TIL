0581ad517648b07e8fe7221e30a32088fed128ac
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2025-05-06 17:45:56 +0900

    [level 3] Title: 네트워크, Time: 9.05 ms, Memory: 9.53 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,23 +1,25 @@
 from collections import deque
 
 def solution(n, computers):
-    visited = [0]*(n)
     answer = 0
+    visited = [[0]*n for _ in range(n)]
     q = deque()
-    def bfs(i):
-        q.append(i)
-        visited[i] = 1
+    def bfs(i,j):
+        q.append((i,j))
+        visited[i][j] = 1
         while q:
-            i = q.popleft()
-            for j in range(n):
-                if computers[i][j] * j != 0: # 연결되있으면 (=1)            
-                    if not visited[computers[i][j]*j]: # 그 중 방문안했으면
-                        q.append(computers[i][j]*j)
-                        visited[computers[i][j]*j] = 1
-                    
+            i, j = q.popleft()
+            for k in range(n):
+                if computers[i][k] == 1 and not visited[i][k]:
+                    q.append((i,k))
+                    visited[i][j] = 1
+                if computers[j][k] == 1 and not visited[j][k]:
+                    q.append((j,k))
+                    visited[j][k] = 1
+            
     for i in range(n):
-        if not visited[i]:
-            bfs(i)
-            answer += 1
-    
+        for j in range(n):
+            if not visited[i][j] and computers[i][j] == 1:
+                bfs(i,j)
+                answer += 1
     return answer
