e6ba066acbb301a0ddf9a9a451b32f1007ac9563
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2025-05-08 18:02:21 +0900

    [level 1] Title: 유연근무제, Time: 5.49 ms, Memory: 9.76 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,24 +1,23 @@
-def add_10_minutes(time):
-    hour = time // 100
-    minute = time % 100
-    minute += 10
-    if minute >= 60:
-        hour += 1
-        minute -= 60
-    return hour * 100 + minute
-
 def solution(schedules, timelogs, startday):
-    answer = 0
-    for i in range(len(schedules)):
-        hope_time = schedules[i]
-        success = True
+    answer = len(schedules)
+    # abs(6-startday), 7-startday는 제외
+    for i in range(len(timelogs)):
         for j in range(7):
-            day = (startday + j - 1) % 7 + 1  # 1~7 사이 요일로 변환
-            if day in (6, 7):  # 토요일, 일요일 제외
+            if (startday + j - 1) % 7 + 1 in (6, 7):
                 continue
-            if timelogs[i][j] > add_10_minutes(hope_time):
-                success = False
-                break
-        if success:
-            answer += 1
+            else:
+                # 평일검사
+                if timelogs[i][j] > 1110:
+                    answer -= 1
+                    break
+                a = str(timelogs[i][j])
+                if a[-2] == '0':
+                    a = str(int(a[:-2])-1)+'5'+a[-1]
+                    if int(a) > schedules[i]:
+                        answer -= 1
+                        break
+                else: 
+                    if timelogs[i][j]-10 > schedules[i]:
+                        answer -= 1
+                        break
     return answer
